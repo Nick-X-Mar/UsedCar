f83d1d2a274a04be9892aeddb696abc672ebdcea
Nick-X-Mar <47928530+Nick-X-Mar@users.noreply.github.com>
2023-08-01 21:39:54 +0300

    φιχ

--- convertDataToXML.js
+++ convertDataToXML.js
@@ -55,7 +55,7 @@ async function convertDataToXML(filePath) {
             let titleText = "";
             // let spanXPath = '//*[@id="__layout"]/div/div[5]/div[3]/div/div/div/section/div/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/div/div/h1/div[1]'
             // let spanXPath = '//*[@id="__layout"]/div/div[5]/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/div/div/h1/div[1]'
-            let spanXPath = '//*[@id="__layout"]/div/div[5]/div[3]/div/div/div/section/div/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/div/div/h1/div[1]'
+            let spanXPath = '//*[@id="__layout"]/div/div[5]/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/div/div/h1/div[1]'
             let spanElement = await page.$x(spanXPath);
             if (spanElement.length === 0) {
                 // Element not found, continue to the next URL
@@ -93,7 +93,7 @@ async function convertDataToXML(filePath) {
 
             let subtitleText = "";
             // let subspanXPath = '//*[@id="__layout"]/div/div[5]/div[3]/div/div/div/section/div/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/div/div/h1/div[2]'
-            let subspanXPath = '//*[@id="__layout"]/div/div[5]/div[3]/div/div/div/section/div/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/div/div/h1/div[2]'
+            let subspanXPath = '//*[@id="__layout"]/div/div[5]/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/div/div/h1/div[2]'
             let subspanElement = await page.$x(subspanXPath);
             try {
                 subtitleText = await page.evaluate(element => element.textContent, subspanElement[0]);
@@ -109,7 +109,7 @@ async function convertDataToXML(filePath) {
             // spanXPath = '//*[@id="__layout"]/div/div[5]/div[3]/div/div/div/section/div/div/div/div/div/div[1]/div[1]/div[7]/div[2]/div/div[1]/div/span'
             // spanXPath = '//*[@id="__layout"]/div/div[5]/div[3]/div/div/div/section/div/div/div/div/div/div[1]/div[1]/div[8]/div[2]/div/div[1]/div/span'
             // spanXPath = '//*[@id="__layout"]/div/div[5]/div/div/div/div/div[1]/div[1]/div[7]/div[2]/div/div[1]/div/span'
-            spanXPath = '//*[@id="__layout"]/div/div[5]/div[3]/div/div/div/section/div/div/div/div/div/div[1]/div[1]/div[8]/div[2]/div/div[1]/div/span'
+            spanXPath = '//*[@id="__layout"]/div/div[5]/div/div/div/div/div[1]/div[1]/div[8]/div[2]/div/div[1]/div/span'
             spanElement = await page.$x(spanXPath);
             try {
                 descriptionText = await page.evaluate(element => element.textContent, spanElement[0]);
